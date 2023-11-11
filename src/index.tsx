c0dbefa7cbdea9d143c4a76455c8c9346fec9435
Ivan Sarabeev <ivan.sarabeev99@gmail.com>
2023-11-11 09:52:22 +0200

    init vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./services/server";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/pages/Van/Navigation/Navigation.tsx
+++ src/pages/Van/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from "../../../components/HTML/Button";
 import { Link } from "react-router-dom";
+import Button from "../../../components/HTML/Button";
 
 const btnTitle = [
   { id: 1, title: "Simple", type: "simple" },
